fc33acf16ef87556cb236bb9f625ee71a49934c0
Rohithmunna31 <rohithkasna31@gmail.com>
2024-07-19 08:53:54 +0530

    Added summary for the website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/layout/header";
+import MealsSummary from "./components/Meals/MealsSummary";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <MealsSummary></MealsSummary>
     </div>
   );
 }
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./MealsSummary.css";
+
+const MealsSummary=()=>{
+
+    return <section className="summary">
+        <h1> Delicious food, Delivered to you</h1>
+        <p> Chooose your favorite meal from our broad selection of available meals
+            and enjoy a Delicious lunch or dinner 
+        </p>
+        <p> 
+            All our meals are cooked with high quality ingredients , just in time and 
+            off course by experienced chefs  
+        </p>
+    </section>
+}
+
+export default MealsSummary;
